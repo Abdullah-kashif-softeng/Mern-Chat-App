3c7c8ee8b9fb60532a3f0359599e460c8bec235c
Abdullah Kashif <abdullah.kashif.softeng@gmail.com>
2025-03-19 23:22:28 +0500

    Started design signup page

--- Frontend/src/pages/SignupPage.jsx
+++ Frontend/src/pages/SignupPage.jsx
@@ -1,7 +1,11 @@
 
 const SignupPage = () => {
   return (
-    <div>SignupPage</div>
+    <div className="flex justify-center items-center ">
+      <div className="bg-amber-700 flex justify-center items-center h-70 w-80">
+        <div className="bg-black">Hello</div>
+      </div>
+    </div>
   )
 }
 
